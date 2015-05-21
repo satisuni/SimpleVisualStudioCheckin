50ed6220c7dddcd025fa20959cac75e8ef902ffa
Satish <gutti80.satish@gmail.com>
2015-05-21 16:17:05 -0700

    first check in

--- simple/simple.Droid/MainActivity.cs
+++ simple/simple.Droid/MainActivity.cs
@@ -14,7 +14,7 @@ namespace simple.Droid
     {
         protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(bundle);           
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
